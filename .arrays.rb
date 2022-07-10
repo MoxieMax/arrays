48eb56546c0ff71ba33d4d79123e67ecb5111437
Max MacGillivray <mary.macgillivray@gmail.com>
2022-07-10 00:54:54 -0700

    Add methods to arrays

--- .arrays.rb
+++ .arrays.rb
@@ -9,3 +9,21 @@ account_balances = [123.45, 3500.76, -28.43, 51.87]
 
 #Boolean variables
 variable = [true, true, false, true]
+
+#.shift will remove the element in the first index position of the array
+names.shift
+
+#.pop will remove the element in the last index position of the array
+vehicle_tires.pop
+
+#.push will add the specified element to the last index position of the array
+account_balance.push(35.23)
+
+#.unshift will add the specified element to the first index position of the array
+variable.unshift(false)
+
+#
+p names
+p vehicle tires
+p account_balances
+p variable
