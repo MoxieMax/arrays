69d5aa34aaa994e1645817eeb5c79756d19c9cb3
Max MacGillivray <mary.macgillivray@gmail.com>
2022-07-10 00:56:15 -0700

    Fixed error in array name

--- .arrays.rb
+++ .arrays.rb
@@ -5,7 +5,7 @@ names = ["Chloe", "Niki", "Naomi", "Kris", "Angela"]
 vehicle_tires = [1, 2, 3, 4, 6]
 
 #Float variables
-account_balances = [123.45, 3500.76, -28.43, 51.87]
+account_balance = [123.45, 3500.76, -28.43, 51.87]
 
 #Boolean variables
 variable = [true, true, false, true]
