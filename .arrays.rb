22b9a355367628d9c48de302101194302bf91382
Max MacGillivray <mary.macgillivray@gmail.com>
2022-07-10 00:41:52 -0700

    Added arrays

---
+++ .arrays.rb
@@ -0,0 +1,11 @@
+#String variables
+names = ["Chloe", "Niki", "Naomi", "Kris", "Angela"]
+
+#Integer variables
+vehicle_tires = [1, 2, 3, 4, 6]
+
+#Float variables
+account_balances = [123.45, 3500.76, -28.43, 51.87]
+
+#Boolean variables
+variable = [true, true, false, true]
